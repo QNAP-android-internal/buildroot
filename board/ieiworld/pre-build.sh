7e8983d62843487b7f761a75093a98cf2dc7aa13
Wig Cheng <wigcheng@ieiworld.com>
2022-08-03 14:12:01 +0800

    board: ieiworld: add test scripts auto-generation in rootfs
    Add panel-screen-test.sh first.

---
+++ board/ieiworld/pre-build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# 2022, Wig Cheng <wigcheng@ieiworld.com>
+BOARD_DIR="$(dirname $0)"
+
+
+# copy test scripts to rootfs
+cp ${BOARD_DIR}/test-additions/panel-screen-test.sh ${TARGET_DIR}/usr/bin/panel-screen-test.sh
---
+++ board/ieiworld/test-additions/panel-screen-test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+fb-test -r
+sleep 2
+fb-test -g
+sleep 2
+fb-test -b
+sleep 2
+fb-test
