b822778fef5a8aa6176600370f3269f8e8bb624d
Wade Li <wadewaldo@gmail.com>
2022-08-17 18:01:52 +0800

    test_lightbar.sh: add this script to test lightbar's led
    pre-build.sh: modify this to move test lightbar's script to output folder

--- board/ieiworld/pre-build.sh
+++ board/ieiworld/pre-build.sh
@@ -6,3 +6,4 @@ BOARD_DIR="$(dirname $0)"
 # copy test scripts to rootfs
 cp ${BOARD_DIR}/test-additions/panel-screen-test.sh ${TARGET_DIR}/usr/bin/panel-screen-test.sh
 cp ${BOARD_DIR}/test-additions/test_buzzer.sh ${TARGET_DIR}/usr/bin/test_buzzer.sh
+cp ${BOARD_DIR}/test-additions/test_lightbar.sh ${TARGET_DIR}/usr/bin/test_lightbar.sh
---
+++ board/ieiworld/test-additions/test_lightbar.sh
@@ -0,0 +1,414 @@
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_00/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_00/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_01/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_01/brightness
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_02/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_02/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_03/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_03/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_00/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_00/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_01/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_01/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_02/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_02/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_03/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_03/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_00/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_00/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_01/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_01/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_02/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_02/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_03/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_03/brightness
+
+
+sleep 0.3s
+
+
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_00/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_00/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_01/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_01/brightness
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_02/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_02/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_03/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_03/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_00/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_00/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_01/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_01/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_02/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_02/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_03/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_03/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_00/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_00/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_01/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_01/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_02/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_02/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_03/brightness
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_03/brightness
+
+
+
+
+
+
+
+
+
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_00/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_01/brightness
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_02/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_03/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_00/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_01/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_02/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_03/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_00/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_01/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_02/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_03/brightness
+
+
+sleep 0.3s
+
+
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_00/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_01/brightness
+
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_02/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_03/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_00/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_01/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_02/brightness
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_03/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_00/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_01/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_02/brightness
+
+
+sleep 0.3s
+
+echo 100 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_03/brightness
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_00/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_01/brightness
+
+
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_02/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_red_03/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_00/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_01/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_02/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_green_03/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_00/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_01/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_02/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/3-0060/leds/i2c4_led_blue_03/brightness
+
+
+sleep 0.3s
+
+
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_00/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_01/brightness
+
+
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_02/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_red_03/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_00/brightness
+
+sleep 0.3s
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_01/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_02/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_green_03/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_00/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_01/brightness
+
+sleep 0.3s
+
+
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_02/brightness
+
+
+sleep 0.3s
+echo 0 > /sys/bus/i2c/drivers/tlc591xx/1-0060/leds/i2c2_led_blue_03/brightness
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
