1e200607458d46ac383ac5bca4bd8045274dc6d6
Wig Cheng <wigcheng@ieiworld.com>
2022-08-25 16:29:12 +0800

    package: emmc-life: add an emmc life time detection tool

---
+++ package/emmc-life/emmc-life.mk
@@ -0,0 +1,14 @@
+EMMC_LIFE_SITE := package/emmc-life
+EMMC_LIFE_SOURCE := emmc-life.c
+EMMC_LIFE_SITE_METHOD := local
+HELLOWORLD_INSTALL_TARGET:=YES
+
+define EMMC_LIFE_BUILD_CMDS
+    "$(TARGET_CC)" -C $(@D)/$(EMMC_LIFE_SOURCE) -o $(@D)/emmc-life
+endef
+
+define EMMC_LIFE_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/emmc-life $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
