cbba25c077d35fe2d03619681288200446f798ea
Wade Li <wadewaldo@gmail.com>
2022-09-29 15:58:00 +0800

    pre-build.sh: implement wifi firmware to buildroot
    AP6275S_firmware/*: add firmware for wifi driver

--- board/ieiworld/pre-build.sh
+++ board/ieiworld/pre-build.sh
@@ -8,3 +8,9 @@ cp ${BOARD_DIR}/test-additions/panel-screen-test.sh ${TARGET_DIR}/usr/bin/panel-
 cp ${BOARD_DIR}/test-additions/test_buzzer.sh ${TARGET_DIR}/usr/bin/test_buzzer.sh
 cp ${BOARD_DIR}/test-additions/test_lightbar.sh ${TARGET_DIR}/usr/bin/test_lightbar.sh
 cp ${BOARD_DIR}/test-additions/check_emmc.sh ${TARGET_DIR}/usr/bin/check_emmc.sh
+
+
+# copy AP6275S_firmware
+cp ${BOARD_DIR}/AP6275S_firmware/fw_bcm43752a2_ag* ${TARGET_DIR}/lib/firmware/
+cp ${BOARD_DIR}/AP6275S_firmware/clm_bcm43752a2_ag.blob ${TARGET_DIR}/lib/firmware/
+cp ${BOARD_DIR}/AP6275S_firmware/nvram_ap6275s.txt ${TARGET_DIR}/lib/firmware/
