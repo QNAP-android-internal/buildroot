6a7d2fb853694dffe231b66a180711ffb0b8568a
Howard Tsou <howardtsou@ieiworld.com>
2022-11-09 10:04:04 +0000

    board : add qc_test services

---
+++ board/ieiworld/qc-additions/main.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+export TERM=linux
+export XDG_RUNTIME_DIR=/run/user/$UID/
+
+config_path="/qc/configs"
+
+if [ -f /tmp/result.txt ];then
+	rm /tmp/result.txt
+fi
+
+i=0
+while true
+do	
+	#collect and integrate status files defined in config file;
+	name=`eval jq '.[$i].names' $config_path/config.json |sed s/\"//g`
+	exec=`eval jq '.[$i].exec' $config_path/config.json |sed s/\"//g`
+	
+	if [ $name == "END" ];then
+		break
+	else
+		#echo ${exec}
+		$exec &
+		echo $name: >>/tmp/result.txt
+	fi
+	
+	i=$(($i+1))		
+done
+
+i=0
+while true
+do
+	name=`eval jq '.[$i].names' $config_path/config.json |sed s/\"//g`
+	statusFile=`eval jq '.[$i].statusFile' $config_path/config.json |sed s/\"//g`	
+	
+	if [ $name == "END" ];then
+		#dialog output
+		dialog_display=`cat /tmp/dialog_display.txt`
+		rm /tmp/dialog_display.txt
+		
+		#echo $dialog_display
+		dialog_cmd="dialog --colors --title 'IEI_Factory_Test' --infobox $dialog_display 50 50 "
+
+		#echo $dialog_cmd
+		$dialog_cmd >/dev/tty1
+		#break
+		i=0
+		sleep 2
+		continue
+	fi
+	status=`cat $statusFile`
+	eval sed -i 's/^$name:.*/$name:$status/g' /tmp/result.txt
+	if [ $status == "pass" ];then
+		echo -n "\Z0\ZB$name:\Z2PASS\n" >>/tmp/dialog_display.txt
+	else
+		echo -n "\Z0\ZB$name:\Z1Failed\n" >>/tmp/dialog_display.txt
+	fi
+	i=$(($i+1))	
+done
--- board/ieiworld/qc-build.sh
+++ board/ieiworld/qc-build.sh
@@ -7,10 +7,21 @@ BOARD_DIR="$(dirname $0)"
 mkdir ${TARGET_DIR}/qc
 cp ${BOARD_DIR}/qc-additions/audio_qc.sh ${TARGET_DIR}/qc/audio_qc.sh
 cp ${BOARD_DIR}/qc-additions/buzzer_qc.sh ${TARGET_DIR}/qc/buzzer_qc.sh
+cp ${BOARD_DIR}/qc-additions/emmc_qc.sh ${TARGET_DIR}/qc/emmc_qc.sh
 cp ${BOARD_DIR}/qc-additions/lcd_qc.sh ${TARGET_DIR}/qc/lcd_qc.sh
+cp ${BOARD_DIR}/qc-additions/main.sh ${TARGET_DIR}/qc/main.sh
+cp ${BOARD_DIR}/qc-additions/mem_qc.sh ${TARGET_DIR}/qc/mem_qc.sh
 cp ${BOARD_DIR}/qc-additions/touch_qc.sh ${TARGET_DIR}/qc/touch_qc.sh
+
+# copy qc config to rootfs
+cp -r ${BOARD_DIR}/qc-additions/configs ${TARGET_DIR}/qc/
 
 # copy AP6275S_firmware
 cp ${BOARD_DIR}/AP6275S_firmware/fw_bcm43752a2_ag* ${TARGET_DIR}/lib/firmware/
 cp ${BOARD_DIR}/AP6275S_firmware/clm_bcm43752a2_ag.blob ${TARGET_DIR}/lib/firmware/
 cp ${BOARD_DIR}/AP6275S_firmware/nvram_ap6275s.txt ${TARGET_DIR}/lib/firmware/
+
+#service
+cp ${BOARD_DIR}/qc-additions/service/qc_test.service ${TARGET_DIR}/etc/systemd/system/qc_test.service
+cp ${TARGET_DIR}/lib/systemd/system/serial-getty@.service ${TARGET_DIR}/lib/systemd/system/serial-getty@tty1.service
+sed -i 's/BindsTo=dev-%i.device/BindsTo=dev-%i/g' ${TARGET_DIR}/lib/systemd/system/serial-getty@tty1.service
