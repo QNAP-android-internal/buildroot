3d93df2ddb9daea85ed53a1e6464354efc1e83ac
Howard Tsou <howardtsou@ieiworld.com>
2022-12-21 18:05:31 +0800

    board: ieiworld: qc-build: add bt firmware

--- board/ieiworld/qc-build.sh
+++ board/ieiworld/qc-build.sh
@@ -29,6 +29,7 @@ cp -r ${BOARD_DIR}/qc-additions/configs ${TARGET_DIR}/qc/
 cp ${BOARD_DIR}/AP6275S_firmware/fw_bcm43752a2_ag* ${TARGET_DIR}/lib/firmware/
 cp ${BOARD_DIR}/AP6275S_firmware/clm_bcm43752a2_ag.blob ${TARGET_DIR}/lib/firmware/
 cp ${BOARD_DIR}/AP6275S_firmware/nvram_ap6275s.txt ${TARGET_DIR}/lib/firmware/
+cp ${BOARD_DIR}/AP6275S_firmware/BCM4362A2_001.003.006.1045.1053.hcd ${TARGET_DIR}/lib/firmware/
 
 #service
 cp ${BOARD_DIR}/qc-additions/service/qc_test.service ${TARGET_DIR}/etc/systemd/system/qc_test.service
