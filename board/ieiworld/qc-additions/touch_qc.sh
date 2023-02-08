ce9cd5c6eac467463ab6c16853bdf29d7bc5d5dd
Howard Tsou <howardtsou@ieiworld.com>
2023-02-08 18:09:50 +0800

    board: ieiworld: touch_qc.sh: fix bug in B664 add calibration

--- board/ieiworld/qc-additions/touch_qc.sh
+++ board/ieiworld/qc-additions/touch_qc.sh
@@ -11,12 +11,13 @@ export TSLIB_TSDEVICE=/dev/input/$EVENT
 export TSLIB_FBDEVICE=/dev/fb0
 export TERM=linux
 export XDG_RUNTIME_DIR=/run/user/$UID/
+export TSLIB_CALIBFILE=/etc/pointercal
 
 if [[ "$(cat /proc/device-tree/model | grep "B643")" ]]; then
 	echo "1563 -51 666480 -58 2535 284402 65536 800 1280 0" > /etc/pointercal
-	export TSLIB_CALIBFILE=/etc/pointercal
+elif [[ "$(cat /proc/device-tree/model | grep "B664")" ]]; then
+	echo "5157 13 -624973 22 3318 -1099304 65536 1280 800 0" > /etc/pointercal
 fi
-
 ts_test
 
 if [[ "$(echo $?)" == "1" ]];then
