df74b6cc0dd1f407012fd3a933e371b0f7f181e9
Wig Cheng <wigcheng@ieiworld.com>
2023-03-15 14:20:42 +0800

    board: ieiworld: qc : correct audio qc for alc5672 sound cards
    Both test for heaset and dmic/speaker sets.

--- board/ieiworld/qc-additions/audio_qc.sh
+++ board/ieiworld/qc-additions/audio_qc.sh
@@ -3,38 +3,103 @@
 export TERM=linux
 export XDG_RUNTIME_DIR=/run/user/$UID/
 
+SOC=`cat /proc/device-tree/model | cut -d ' ' -f3`
+if [ $SOC == "B664" ];then
+	AUD_CARD=0
+elif [ $SOC == "B643" ];then
+	AUD_CARD=1
+fi
 
-if [ -f /sys/class/i2c-dev/i2c-2/device/2-0010/ ]; then
+if [ -f /sys/class/i2c-dev/i2c-2/device/2-001c/ ]; then
     echo "codec existed, starting test"
 else
     echo "fail" > /tmp/audio_qc.txt
 fi
 
-amixer sset 'Volume Adjustment Smoothing' 'off'  
-amixer sset 'DAC Mono Mode' 'on' 
-amixer sset 'LINEA Mixer IN5' 'on'
-amixer sset 'Left ADC Mixer LINEA' 'on'
+# for alc5672
+amixer sset 'IN1 Boost' '2'
+amixer sset 'RECMIXL BST1' 'on'
+amixer sset 'RECMIXR BST1' 'on'
+amixer sset 'Sto1 ADC MIXL ADC1' 'on'
+amixer sset 'Sto1 ADC MIXL ADC2' 'on'
+amixer sset 'Sto1 ADC MIXR ADC1' 'on'
+amixer sset 'Sto1 ADC MIXR ADC2' 'on'
+amixer sset 'Sto2 ADC MIXL ADC1' 'on'
+amixer sset 'Sto2 ADC MIXL ADC2' 'on'
+amixer sset 'Sto2 ADC MIXR ADC1' 'on'
+amixer sset 'Sto2 ADC MIXR ADC2' 'on'
+amixer sset 'Stereo DAC MIXL DAC L1' 'on'
+amixer sset 'Stereo DAC MIXR DAC R1' 'on'
 
-arecord -f dat | aplay -f dat &
+amixer sset 'HPOVOL MIXL DAC1' 'on'
+amixer sset 'HPOVOL MIXR DAC1' 'on'
+amixer sset 'HPO MIX HPVOL' 'on'
+amixer sset 'PDM1 L Mux' 'Stereo DAC'
+amixer sset 'PDM1 R Mux' 'Stereo DAC'
 
-sleep 2
+
+sh -c 'dialog --colors --title "Headset Test" \
+--no-collapse --yesno "\nStart test when you choose \"Yes\" \nListening about 5 secs..." 10 50 \
+<> /dev/tty1 >&0 2>&1'
+
+arecord -D hw:$AUD_CARD -f dat | aplay -D hw:$AUD_CARD -f dat &
+
+sleep 5
 
 REC_PID=$(ps | grep arecord | head -1 | awk  '{print $1}')
 PLAY_PID=$(ps | grep aplay | head -1 | awk  '{print $1}')
 
-sh -c 'dialog --colors --title "Audio Test" \
---no-collapse --yesno "Hear MIC sound?" 10 50 \
-<> /dev/tty1 >&0'
+sh -c 'dialog --colors --title "Headset Test" \
+--no-collapse --yesno "Hear loopback sound?" 10 50 \
+<> /dev/tty1 >&0 2>&1'
 
 AUDIO_RESULTS="$?"
 if [[ "$AUDIO_RESULTS" == '1' ]]; then
         echo "fail" > /tmp/audio_qc.txt
+	kill -9 $REC_PID
+	kill -9 $PLAY_PID
+	dd if=/dev/zero of=/dev/fb0
+	kill -9 $$
+	exit
+fi
+
+kill -9 $REC_PID
+kill -9 $PLAY_PID
+
+sh -c 'dialog --colors --title "DMIC/Speaker Test" \
+--no-collapse --yesno "\nStart test when you choose \"Yes\" \nListening about 5 secs...\nPlease plug-out headset first" 10 50 \
+<> /dev/tty1 >&0 2>&1'
+
+if [ $SOC == "B664" ];then
+	arecord -D hw:$AUD_CARD -f dat | aplay -D hw:$AUD_CARD -f dat &
+elif [ $SOC == "B643" ];then
+        speaker-test -t wav -c 2 -D hw:$AUD_CARD
+fi
+
+sleep 5
+
+REC_PID=$(ps | grep arecord | head -1 | awk  '{print $1}')
+PLAY_PID=$(ps | grep aplay | head -1 | awk  '{print $1}')
+SPK_PID=$(ps | grep speaker-test | head -1 | awk  '{print $1}')
+
+sh -c 'dialog --colors --title "DMIC/Speaker Test" \
+--no-collapse --yesno "Hear sound?" 10 50 \
+<> /dev/tty1 >&0 2>&1'
+
+AUDIO_RESULTS="$?"
+if [[ "$AUDIO_RESULTS" == '1' ]]; then
+        echo "fail" > /tmp/audio_qc.txt
+	kill -9 $REC_PID
+	kill -9 $PLAY_PID
+	kill -9 $SPK_PID
 elif [[ "$AUDIO_RESULTS" == '0' ]]; then
         echo "pass" > /tmp/audio_qc.txt
 fi
 
 kill -9 $REC_PID
 kill -9 $PLAY_PID
+kill -9 $SPK_PID
+
 dd if=/dev/zero of=/dev/fb0
 
 kill -9 $$
