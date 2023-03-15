0462d0418723fd006349c349a8f7cc948c9930da
Howard Tsou <howardtsou@ieiworld.com>
2023-03-15 15:34:31 +0800

    board: ieiworld: qc : add bt ble test use bluetoothctl instead of hcitool for both BT classic and ble devices

--- board/ieiworld/qc-additions/bt_qc.sh
+++ board/ieiworld/qc-additions/bt_qc.sh
@@ -4,8 +4,8 @@ if [ -f /tmp/bt_qc.txt ];then
         rm /tmp/bt_qc.txt
 fi
 
-if [ -f /tmp/bt_scan.txt ];then
-        rm /tmp/bt_scan.txt
+if [ -f /tmp/btctl_scan.txt ];then
+        rm /tmp/btctl_scan.txt
 fi
 
 if [ -f /tmp/bt_ping.txt ];then
@@ -18,21 +18,47 @@ baudrate=115200
 brcm_patchram_plus  --enable_hci --no2bytes --use_baudrate_for_download --tosleep 200000 --baudrate $baudrate --patchram $bt_firmware_path /dev/ttymxc0 &
 
 sleep 10
-
 hciconfig hci0 up
-sleep 2
+
+bluetoothctl power on
+bluetoothctl agent on
+bluetoothctl scan on >/tmp/btctl_scan.txt &
+
+bt_name="$1"
+echo "$bt_name" > /tmp/bt_name.txt
+
+check_bt_info=false
 while true
 do
-	hcitool scan > /tmp/bt_scan.txt ;sync
-	sleep 2
-	cat /tmp/bt_scan.txt |grep "$1"
+	cat /tmp/btctl_scan.txt |grep "$bt_name"
 	if [ $? == 0 ];then
-		bt_mac=`cat /tmp/bt_scan.txt |grep "$1" |awk  '{print $1}'`
-		l2ping -c10 -t1 $bt_mac >/tmp/bt_ping.txt ;sync
-		cat /tmp/bt_ping.txt |grep "0% loss"
+		bt_mac=`cat /tmp/btctl_scan.txt |grep "$bt_name" |awk '{print $3}'`
+		bluetoothctl info $bt_mac >/tmp/bt_info.txt
+		cat /tmp/bt_info.txt |grep "Name: $bt_name"
 		if [ $? == 0 ];then
+			check_bt_info=true
+		fi
+	fi	
+	if $check_bt_info;then
+		#hcitool scan for BT Classic to test l2ping
+		hcitool scan > /tmp/bt_hci_scan.txt ;sync
+		sleep 2
+		cat /tmp/bt_hci_scan.txt |grep "$bt_name"
+		if [ $? == 0 ];then
+			#classic
+			l2ping -c10 -t1 $bt_mac >/tmp/bt_ping.txt ;sync
+			cat /tmp/bt_ping.txt |grep "0% loss"
+	                if [ $? == 0 ];then
+        	                echo pass >/tmp/bt_qc.txt
+                	        break
+                	fi
+		else
+			#ble 
 			echo pass >/tmp/bt_qc.txt
-			break
-		fi			
+                        break
+		fi
 	fi
 done
+
+btctl_scan_pid=`ps |grep "bluetoothctl scan on" |grep -v "grep" |awk '{print $1}'`
+kill $btctl_scan_pid
