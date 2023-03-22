07910f8286ef39758b64d834a833e4226da7242c
Howard Tsou <howardtsou@ieiworld.com>
2023-03-22 17:01:16 +0800

    board: ieiworld: qc: hdmi_qc.sh: change resolution when fb-test to fit hdmi monitor size

--- board/ieiworld/qc-additions/hdmi_qc.sh
+++ board/ieiworld/qc-additions/hdmi_qc.sh
@@ -7,7 +7,9 @@ if [ -f /tmp/edid_tmp ];then
         rm /tmp/edid_tmp
 fi
 
-#fbset -fb /dev/fb0 -g 1920 1080 1920 1080 16
+res_org=`fbset |grep geometry |cut -d ' ' -f 1 --complement`
+echo "$res_org" >/tmp/res.txt
+
 hexdump /sys/devices/platform/display-subsystem/drm/card1/card1-HDMI-A-1/edid >/tmp/edid_tmp
 edid_size=`du /tmp/edid_tmp | awk '{print $1}'`
 if [ $edid_size == 0 ];then
@@ -16,11 +18,12 @@ if [ $edid_size == 0 ];then
 	return 1
 fi
 
+fbset -fb /dev/fb0 -g 1920 1080 1920 1080 16
 fb-test
 sleep 5
 
 sh -c 'dialog --colors --title "HDMI Test" \
---no-collapse --yesno "Is RGB work?" 10 50 \
+--no-collapse --yesno "Is HDMI RGB work?" 10 50 \
 <> /dev/tty1 >&0'
 
 if [ $? == 0 ];then
@@ -31,5 +34,6 @@ fi
 cat /tmp/hdmi_qc.txt
 
 dd if=/dev/zero of=/dev/fb0
-kill -9 $$
+fbset -fb /dev/fb0 -g $res_org
 
+kill -9 $$
