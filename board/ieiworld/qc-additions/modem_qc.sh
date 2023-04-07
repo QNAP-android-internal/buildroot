40b37ffb63e9a18127d469f7c9c5faa11950ba38
Wig Cheng <wigcheng@ieiworld.com>
2023-04-07 13:34:45 +0800

    board: ieiworld: modem_qc.sh: simplied test flow for module only

--- board/ieiworld/qc-additions/modem_qc.sh
+++ board/ieiworld/qc-additions/modem_qc.sh
@@ -1,36 +1,16 @@
 #!/bin/sh
 
 echo "fail" > /tmp/modem_qc.txt
-CNT=0
-CNT_MAX=25
 
 while true
 do
-	MODULE=$(mmcli -m 0 | grep "h/w revision" | awk -F: '{print $2}')
-	if [[ "$MODULE" == " EM7455" ]];then
-		break
-	else
-		if [[ "$CNT" == "$CNT_MAX" ]];then
-			echo "fail" > /tmp/modem_qc.txt
-			systemctl stop NetworkManager
-			exit
-		fi
-	fi
+        MODULE=$(mmcli -m 0 | grep "h/w revision" | awk -F: '{print $2}')
+        if [[ "$MODULE" == " EM7455" ]];then
+                echo "pass" > /tmp/modem_qc.txt
+                break
+        fi
 
-	CNT=$((CNT+1))
-	sleep 1
+        sleep 1
 done
-
-if [[ "$(mmcli -m 0 | grep "sim-missing")" ]];then
-	echo "fail" > /tmp/modem_qc.txt
-else
-
-	if [[ $(mmcli -m 0 | grep own | awk -F: '{print $2}' | grep "+8") ]];then
-		# make sure phone number is +886/+86
-		echo "pass" > /tmp/modem_qc.txt
-	else
-		echo "fail" > /tmp/modem_qc.txt
-	fi
-fi
 
 systemctl stop NetworkManager
