d9de71c87d1f0d0cb11ab73a831bf48dd28e7539
Wig Cheng <wigcheng@ieiworld.com>
2023-04-12 01:09:26 +0800

    board: ieiworld: qc: change image location from emmc to usb storage

--- board/ieiworld/qc-additions/flash_image.sh
+++ board/ieiworld/qc-additions/flash_image.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-IMAGE_NAME=$(ls /qc/image/android/)
-
 if [[ "$(cat /proc/device-tree/model | grep "B643")" ]]; then
-    IMAGE_SIZE=13958643712
+        IMAGE_SIZE=13958643712
+        FOLDER="b643"
 elif [[ "$(cat /proc/device-tree/model | grep "B664")" ]]; then
-    IMAGE_SIZE=30064771072
+        IMAGE_SIZE=30064771072
+        FOLDER="b664"
 else
-    IMAGE_SIZE=13958643712
+        IMAGE_SIZE=13958643712
+        FOLDER="b643"
 fi
 
-xzcat /qc/image/android/"$IMAGE_NAME" | (pv -n -s "$IMAGE_SIZE" | dd of=/dev/mmcblk2 bs=1M) 2>&1 | tee /tmp/flash_progress.txt
+IMAGE_NAME=$(ls /mnt/"$FOLDER")
+xzcat /mnt/"$FOLDER"/"$IMAGE_NAME" | (pv -n -s "$IMAGE_SIZE" | dd of=/dev/mmcblk2 bs=1M) 2>&1 | tee /tmp/flash_progress.txt
