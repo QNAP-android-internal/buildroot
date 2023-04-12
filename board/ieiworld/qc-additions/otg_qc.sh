45ec80164f0dafb4f30d17e2e5aecc8bea930df5
Wig Cheng <wigcheng@ieiworld.com>
2023-04-12 18:24:54 +0800

    board: ieiworld: otg_qc: change to scan host side until success

--- board/ieiworld/qc-additions/otg_qc.sh
+++ board/ieiworld/qc-additions/otg_qc.sh
@@ -15,26 +15,30 @@ case $SOC in
 		;;
 esac
 
-cat /proc/interrupts | grep "$IRQ_name"
-if [ $? != 0 ];then
-	IRQ_times_before=0
-else
-	IRQ_times_before=`cat /proc/interrupts | grep "$IRQ_name" | awk '{print $2}'`
-fi
-modprobe g_serial
-sleep 1
+while true
+do
+	cat /proc/interrupts | grep "$IRQ_name"
+	if [ $? != 0 ];then
+		IRQ_times_before=0
+	else
+		IRQ_times_before=`cat /proc/interrupts | grep "$IRQ_name" | awk '{print $2}'`
+	fi
+	modprobe g_serial
+	sleep 1
 
-IRQ_times_after=`cat /proc/interrupts | grep "$IRQ_name" | awk '{print $2}'`
+	IRQ_times_after=`cat /proc/interrupts | grep "$IRQ_name" | awk '{print $2}'`
 
-echo "IRQ_times_before=$IRQ_times_before"
-echo "IRQ_times_after=$IRQ_times_after"
+	echo "IRQ_times_before=$IRQ_times_before"
+	echo "IRQ_times_after=$IRQ_times_after"
  
-times=`expr $IRQ_times_after - $IRQ_times_before`
-echo "times=$times
-"
-if [ $times -gt 0 ];then
-        echo pass >/tmp/otg_qc.txt
-else
-        echo fail >/tmp/otg_qc.txt
-fi
-cat /tmp/otg_qc.txt
+	times=`expr $IRQ_times_after - $IRQ_times_before`
+	echo "times=$times"
+
+	if [ $times -gt 0 ];then
+		echo pass >/tmp/otg_qc.txt
+		exit
+	else
+		echo fail >/tmp/otg_qc.txt
+	fi
+	cat /tmp/otg_qc.txt
+done
