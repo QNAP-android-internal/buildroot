90964bd274edade9dba034cde033cedddbece791
Wig Cheng <wigcheng@ieiworld.com>
2023-04-12 18:09:04 +0800

    board: ieiworld: usb_qc: change a way for usb testing
    scan two usb ports have device or not because the device could be
    storage disk, or hid device.

--- board/ieiworld/qc-additions/usb_qc.sh
+++ board/ieiworld/qc-additions/usb_qc.sh
@@ -4,24 +4,38 @@ if [ -f /tmp/usb_qc.txt ];then
 	rm /tmp/usb_qc.txt
 fi
 
-if [ -f /tmp/usb_tmp.txt ];then
-        rm /tmp/usb_tmp.txt
-fi
-
 while true
 do
-	ls /dev/sda* > /tmp/usb_tmp.txt &
-	sleep 1
-	grep sda /tmp/usb_tmp.txt
-	if [ $? == 0 ];then
-		echo pass > /tmp/usb_qc.txt
-		echo "usb pass"
-		break
+	USB_CNT=0
+
+	if [[ "$(lsusb -t | grep "Port 3")" ]]; then
+		USB_CNT=$(($USB_CNT+1))
+	fi
+
+	if [[ "$(lsusb -t | grep "Port 4")" ]]; then
+		USB_CNT=$(($USB_CNT+1))
+	fi
+
+	if [[ "$USB_CNT" == "2" ]]; then
+		echo "pass stage 1"
 	else
 		echo fail > /tmp/usb_qc.txt
-		sleep 2
+		sleep 3
+		continue
 	fi
+
+	PORT3_CNT=$(lsusb -t | grep "Port 3" | wc -l)
+	PORT4_CNT=$(lsusb -t | grep "Port 4" | wc -l)
+	PORT_TOTAL=$(($PORT3_CNT+$PORT4_CNT))
+
+	STORAGE_CNT=$(lsusb -t | grep "usb-storage" | wc -l)
+	HID_CNT=$(lsusb -t | grep "usbhid" | wc -l)
+	TOTAL_CNT=$(($STORAGE_CNT+$HID_CNT))
+
+	if [[ "$TOTAL_CNT" == "$PORT_TOTAL" ]]; then
+		echo pass > /tmp/usb_qc.txt
+	else
+		echo fail > /tmp/usb_qc.txt
+	fi
+	sleep 3
 done
-
-rm /tmp/usb_tmp.txt
-
