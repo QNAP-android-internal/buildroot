0f9de6b0472148507e237c036fac79c9b753d19a
Wig Cheng <wigcheng@ieiworld.com>
2023-04-13 10:52:22 +0800

    board: ieiworld: qc: improve flash_image process
    Move UI control from main.sh to flash_image.sh, and add usb storage
    detection for usb installer way.

--- board/ieiworld/qc-additions/flash_image.sh
+++ board/ieiworld/qc-additions/flash_image.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+export TERM=linux
+export XDG_RUNTIME_DIR=/run/user/$UID/
+
+if [[ $(ls /dev/sda*) ]];then
+        echo "GET USB INSTALLER"
+        mount /dev/sda1 /mnt
+else
+        echo "NO USB INSTALLER"
+        sh -c 'dialog --colors --title "ANDROID FLASHING" \
+        --no-collapse --msgbox "NO DETECT USB INSTALLER\nPlease reboot and try again" 10 50 \
+        <> /dev/tty1 >&0 2>&1'
+        exit
+fi
+
+
 if [[ "$(cat /proc/device-tree/model | grep "B643")" ]]; then
         IMAGE_SIZE=13958643712
         FOLDER="b643"
@@ -12,4 +27,30 @@ else
 fi
 
 IMAGE_NAME=$(ls /mnt/"$FOLDER")
-xzcat /mnt/"$FOLDER"/"$IMAGE_NAME" | (pv -n -s "$IMAGE_SIZE" | dd of=/dev/mmcblk2 bs=1M) 2>&1 | tee /tmp/flash_progress.txt
+(xzcat /mnt/"$FOLDER"/"$IMAGE_NAME" | (pv -n -s "$IMAGE_SIZE" | dd of=/dev/mmcblk2 bs=1M) 2>&1 | tee /tmp/flash_progress.txt)&
+
+sleep 1
+
+while true
+do
+        if [ -f /tmp/flash_progress.txt ];then
+                percentage=`cat /tmp/flash_progress.txt| tail -n1`
+                echo $percentage |grep "copied,"
+                if [ $? == 0 ];then
+                        percentage=100
+                fi
+        else
+                percentage=0
+        fi
+
+        echo $percentage | dialog --gauge "Flashing Android image, please wait" 10 70 70 > /dev/tty1
+        if [ $percentage -ge 100 ];then
+                break
+        fi
+
+        sleep 3
+done
+
+sync
+echo $percentage | dialog --gauge "Finish! please reboot into Android OS" 10 70 70 > /dev/tty1
+umount /mnt
