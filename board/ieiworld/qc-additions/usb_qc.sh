282b596ef05b19410a7e67f97903c91d39528074
Wig Cheng <wigcheng@ieiworld.com>
2023-04-13 18:20:21 +0800

    board: ieiworld: qc: improve usb_qc to make b664 and b643 compatible

--- board/ieiworld/qc-additions/usb_qc.sh
+++ board/ieiworld/qc-additions/usb_qc.sh
@@ -4,15 +4,29 @@ if [ -f /tmp/usb_qc.txt ];then
 	rm /tmp/usb_qc.txt
 fi
 
+PORT_A="Port 1"
+PORT_B="Port 2"
+
 while true
 do
 	USB_CNT=0
 
-	if [[ "$(lsusb -t | grep "Port 3")" ]]; then
+	if [[ "$(cat /proc/device-tree/model | grep "B643")" ]]; then
+		PORT_A="Port 3"
+		PORT_B="Port 4"
+	elif [[ "$(cat /proc/device-tree/model | grep "B664")" ]]; then
+		PORT_A="Port 1"
+		PORT_B="Port 2"
+	else
+		PORT_A="Port 3"
+		PORT_B="Port 4"
+	fi
+
+	if [[ "$(lsusb -t | grep "$PORT_A")" ]]; then
 		USB_CNT=$(($USB_CNT+1))
 	fi
 
-	if [[ "$(lsusb -t | grep "Port 4")" ]]; then
+	if [[ "$(lsusb -t | grep "$PORT_B")" ]]; then
 		USB_CNT=$(($USB_CNT+1))
 	fi
 
@@ -24,9 +38,11 @@ do
 		continue
 	fi
 
-	PORT3_CNT=$(lsusb -t | grep "Port 3" | wc -l)
-	PORT4_CNT=$(lsusb -t | grep "Port 4" | wc -l)
-	PORT_TOTAL=$(($PORT3_CNT+$PORT4_CNT))
+	PORTA_STORAGE_CNT=$(lsusb -t | grep "$PORT_A" | grep "usb-storage" | wc -l)
+	PORTA_HID_CNT=$(lsusb -t | grep "$PORT_A" | grep "usbhid" | wc -l)
+	PORTB_STORAGE_CNT=$(lsusb -t | grep "$PORT_B" | grep "usb-storage" | wc -l)
+	PORTB_HID_CNT=$(lsusb -t | grep "$PORT_B" | grep "usbhid" | wc -l)
+	PORT_TOTAL=$(($PORTA_STORAGE_CNT+$PORTA_HID_CNT+$PORTB_STORAGE_CNT+$PORTB_HID_CNT))
 
 	STORAGE_CNT=$(lsusb -t | grep "usb-storage" | wc -l)
 	HID_CNT=$(lsusb -t | grep "usbhid" | wc -l)
